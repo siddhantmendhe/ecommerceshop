9711e6c4dfcc0952a6086122a82b63ae7e2624a6
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-09-05 12:46:22 +0530

    added logo in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 
 const Header = () => {
@@ -8,7 +9,9 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">EcommerceShop</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={logo} alt="" />
+                     EcommerceShop</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className="ms- auto">
