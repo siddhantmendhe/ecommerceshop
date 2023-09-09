7ae983a92ecb758cddb742f939e99bcb379aa38f
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-09-09 17:52:50 +0530

    star ratings added below title of products

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import Rating from './Rating'
 const Product = ({product}) => {
   return (
     <Card className='my-3 p-3 rounded'>
@@ -10,10 +11,14 @@ const Product = ({product}) => {
 
         <Card.Body>
             <a href={`/product/${product._id}`}>
-                <Card.Title as="div">
+                <Card.Title as="div" className='product-title'>
                     <strong>{product.name}</strong>
                 </Card.Title>
             </a>
+
+            <Card.Text as='div'>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+            </Card.Text>
             <Card.Text as="h3">
                 ${product.price}
             </Card.Text>
