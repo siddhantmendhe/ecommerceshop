d891a28c7b50d695a0ae0c72340a14aafbf5a907
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-09-11 17:22:35 +0530

    deleted products.js file from frontend, now fetching data from server api
    previously we used to fetch data from products.js files in the frontend folder, with client-side data fetching.
    Now we are using Axios to fetch data from server APIs and storing data in states.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import products from '../product';
+import {React, useEffect, useState }from 'react'
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from '../components/Rating';
 const ProductScreen = () => {
+    const [product, setProduct]= useState([]);
+    
     const {id: productId}=useParams();
-    const product=products.find((p)=>p._id===productId);
-    console.log(product);
+    useEffect(()=>{
+        const fetchProducts =async ()=>{
+          const {data} = await axios.get(`/api/products/${productId}`);
+          setProduct(data);
+        };
+    
+        fetchProducts();
+      }, [productId]);
   return (
     <>
     <Link className='btn btn-light my-3' to='/'>Go Back</Link>
