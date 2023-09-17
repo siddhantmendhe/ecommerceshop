2da3e00118f19c15517c4db7b3b58957a5af6db9
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-09-17 22:48:56 +0530

    hashing password
    hashing using bcryptjs. bcrypt.hash(pass,10) returns promise.

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,21 +1,21 @@
-
+import bcrypt from 'bcryptjs'
 
 const users = [
   {
     name: 'Admin User',
     email: 'admin@email.com',
-    password: 123456,
+    password: await bcrypt.hash('123456',10) ,
     isAdmin: true,
   },
   {
     name: 'John Doe',
     email: 'john@email.com',
-    password: 123456,
+    password: await bcrypt.hash('123456',10),
   },
   {
     name: 'Jane Doe',
     email: 'jane@email.com',
-    password: 123456,
+    password: await bcrypt.hash('123456',10),
   },
 ];
 
