3673832c60523da9aceb76784c5658e3e8484722
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-09-19 19:21:10 +0530

    implemented express Routers for server file

---
+++ backend/routs/productRoutes.js
@@ -0,0 +1,16 @@
+import  express  from 'express';
+const router=express.Router();
+// .js is important because we are using ES module in backend
+import products from '../data/products.js';
+
+
+
+router.get('/', (req, res)=>{
+    res.json(products);
+});
+
+router.get('/:id', (req, res)=>{
+    const product=products.find((p)=>p._id===req.params.id);
+    res.json(product);
+});
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import productRoutes from './routs/productRoutes.js'
 dotenv.config();
-// .js is important because we are using ES module in backend
-import products from './data/products.js';
+
 const port = process.env.PORT || 5000;
 connectDB();
 const app= express();
@@ -11,15 +11,8 @@ const app= express();
 app.get('/', (req, res)=>{
     res.send('API is runnig...');
 });
+app.use('/api/products',productRoutes)
 
-app.get('/api/products', (req, res)=>{
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res)=>{
-    const product=products.find((p)=>p._id===req.params.id);
-    res.json(product);
-});
 
 app.listen(port, ()=>{console.log(`Server running on port ${port}`)});
 
