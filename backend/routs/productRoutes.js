ee19cf43ba331d1ded3dd1ee39a2e9e6a085cd52
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-10-07 10:59:00 +0530

    fetching products and product details from database

--- backend/routs/productRoutes.js
+++ backend/routs/productRoutes.js
@@ -2,15 +2,24 @@ import  express  from 'express';
 const router=express.Router();
 // .js is important because we are using ES module in backend
 import products from '../data/products.js';
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js'
 
 
 
-router.get('/', (req, res)=>{
+router.get('/', asyncHandler(async (req, res)=>{
+    const products =await Product.find({});
     res.json(products);
-});
+}));
 
-router.get('/:id', (req, res)=>{
-    const product=products.find((p)=>p._id===req.params.id);
-    res.json(product);
-});
+router.get('/:id', 
+    asyncHandler(async (req, res)=>{
+    const product= await Product.findById(req.params.id);
+
+    if(product){
+        return res.json(product);
+
+    }
+    res.status(404).json({message:'Product not found'});
+}));
 export default router;
