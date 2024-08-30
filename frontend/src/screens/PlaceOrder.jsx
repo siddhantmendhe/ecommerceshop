bf5a0cd82b5c02f7a8175cf64403b76086029117
Siddhant Mendhe <sinddhant.mendhe11@gmail.com>
2024-08-30 17:02:55 +0530

    place order screen in progress

--- frontend/src/screens/PlaceOrder.jsx
+++ frontend/src/screens/PlaceOrder.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const PlaceOrder = () => {
+    const cart= useSelector(state=> state.cart)
+    const dispatch= useDispatch()
+    const navigate= useNavigate();
+
+    console.log(cart);
+    useEffect(()=>{
+        if(!cart.shippingAddress){
+            navigate('/shopping')
+
+        }
+        if(!cart.paymentMethod){
+            navigate('/payment')
+
+        }
+    },[cart.shippingAddress, cart.paymentMethod,navigate])
   return (
     <div>
       <h1>place order</h1>
