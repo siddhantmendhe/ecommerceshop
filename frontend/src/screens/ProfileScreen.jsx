85b1b22dbd3c773c7ea407d3c1afd8bbc9f98626
Siddhant Mendhe <38434303+siddhantmendhe@users.noreply.github.com>
2024-09-03 16:15:43 +0530

    display order history table showing logged in user orders is completed

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -8,6 +8,7 @@ import {  } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { useProfileUpdateMutation } from '../slices/userApiSlice'
 import { setCredentials } from '../slices/authSlice'
+import { useGetMyOrdersQuery } from '../slices/orderApiSlice'
 
 const ProfileScreen = () => {
     const [alert, setAlert]=useState(false); // to track alert
@@ -30,6 +31,8 @@ const ProfileScreen = () => {
 
     //update profile mutaion 
     const [profileUpdate, {isLoading:loadingUpdateProfile}]= useProfileUpdateMutation();
+    //get user orders query hook
+    const { data:orders, isLoading, error}= useGetMyOrdersQuery();
   
    
     
@@ -118,8 +121,58 @@ const ProfileScreen = () => {
       </Form>
     </Col>
     <Col md={9}>
-     
-  
+    <h2>My Orders</h2>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>
+            {error?.data?.message || error.error}
+          </Message>
+        ) : (
+          <Table  striped bordered hover>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th>Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <FaTimes style={{ color: 'red' }} />
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button className='btn-sm' variant='light'>
+                        Details
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+
     </Col>
   </Row>
   )
