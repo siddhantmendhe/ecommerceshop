0742f6d93fb696960a673162a9b6383d48ec731a
Siddhant Mendhe <38434303+siddhantmendhe@users.noreply.github.com>
2024-09-14 14:52:11 +0530

    no need for login for carousels

--- backend/routs/productRoutes.js
+++ backend/routs/productRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import { createProduct,  createProductReview,  deleteProduct, editProduct, getProduct, getProducts, getTopProducts } from '../controllers/productContoller.js';
 
 router.route('/').get(getProduct).post(protect, admin,createProduct);
-router.route('/top').get(protect,getTopProducts)
+router.route('/top').get(getTopProducts)
 
 router.route('/:id').get(getProducts).put(protect,admin, editProduct).delete(protect,admin, deleteProduct);
 router.route('/:id/reviews').post(protect,createProductReview);
