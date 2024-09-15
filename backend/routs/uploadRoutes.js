65315e9f8eeb6098324e556b564a7e6239989b19
Siddhant Mendhe <38434303+siddhantmendhe@users.noreply.github.com>
2024-09-15 13:11:43 +0530

    file upload route updated

--- backend/routs/uploadRoutes.js
+++ backend/routs/uploadRoutes.js
@@ -12,26 +12,32 @@ const storage=multer.diskStorage({
     }
 });
 
-function checkFileType(file,cb){
-    const fileTypes=/jpg|jpeg|png/;
-    const extname=filetypes.test(path.extname(file.originalname).toLowerCase());
-    const minetype=filetpes.test(file.mimetype);
-    if(extname&&minetype){
-        return cb(null, true);
-    }else{
-        cb('Images only !');
+function fileFilter(req, file, cb) {
+    const filetypes = /jpe?g|png|webp/;
+    const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+  
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = mimetypes.test(file.mimetype);
+  
+    if (extname && mimetype) {
+      cb(null, true);
+    } else {
+      cb(new Error('Images only!'), false);
     }
-}
+  }
 
-const upload=multer({
-    storage,
-})
-router.post('/',upload.single('image'),(req,res)=>{
-
-    res.json({
-        message:'Image Upload',
-       
-        image:`/${req.file.path}`
-    })
-})
+  const upload = multer({ storage, fileFilter });
+  const uploadSingleImage = upload.single('image');
+  router.post('/', (req, res) => {
+    uploadSingleImage(req, res, function (err) {
+      if (err) {
+        res.status(400).send({ message: err.message });
+      }
+  
+      res.status(200).send({
+        message: 'Image uploaded successfully',
+        image: `/${req.file.path}`,
+      });
+    });
+  });
 export default router;
